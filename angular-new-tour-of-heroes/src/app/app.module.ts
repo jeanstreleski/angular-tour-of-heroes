9df0f85040073988f5cd552dc337f14ffb09b1c7
jeanstreleski <jeanstreleski@192.168.0.22>
2018-07-21 11:47:12 -0300

    ok

--- angular-new-tour-of-heroes/src/app/app.module.ts
+++ angular-new-tour-of-heroes/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
