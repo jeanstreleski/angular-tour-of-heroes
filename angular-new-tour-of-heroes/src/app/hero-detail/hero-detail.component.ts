b87ed28427195615043f0cf4691aa90567df76a5
jeanstreleski <jeanstreleski@192.168.0.17>
2018-08-02 22:28:09 -0300

    ok

--- angular-new-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-new-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,13 +1,14 @@
-import {Component, OnInit, Input} from '@angular/core';
-import {Hero} from '../hero';
-import {ActivatedRoute} from '@angular/router';
-import {Location} from '@angular/common';
-import {HeroService} from '../hero.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero }         from '../hero';
+import { HeroService }  from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
@@ -32,4 +33,8 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+ save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
